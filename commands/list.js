d734e6b5e12b7c4915bc3d718ba1c2cddf72712f
Breyana Scales <breyanaj@gmail.com>
2017-08-03 15:31:41 -0700

    Implement list functionality

--- commands/list.js
+++ commands/list.js
@@ -1,21 +1,22 @@
-const fs = require('fs')
-const readTasks = require('../readTasks.js')
+const { listAllFromDB } = require('../db')
+
 
 const list = () => {
-  readTasks((tasks) => {
-    const widestIDString = Math.max.apply(null, tasks.map(task => task.id.toString().length))
+  listAllFromDB()
 
-    if (widestIDString <= 0) {
-      console.log("There are no tasks.")
-      return
-    }
-
-    console.log("ID" + " ".repeat(widestIDString) + "Description")
-    console.log("-".repeat(widestIDString + 1) + " " + "-".repeat(11))
-    tasks.forEach((task) => {
-      console.log(task.id + " ".repeat(widestIDString + 2 - task.id.toString().length) + task.task)
-    })
-  })
+    //
+    // const widestIDString = Math.max.apply(null, tasks.map(task => task.id.toString().length))
+    //
+    // if (widestIDString <= 0) {
+    //   console.log("There are no tasks.")
+    //   return
+    // }
+    //
+    // console.log("ID" + " ".repeat(widestIDString) + "Description")
+    // console.log("-".repeat(widestIDString + 1) + " " + "-".repeat(11))
+    // tasks.forEach((task) => {
+    //   console.log(task.id + " ".repeat(widestIDString + 2 - task.id.toString().length) + task.task)
+    // })
 }
 
 module.exports = list
--- db/index.js
+++ db/index.js
@@ -7,11 +7,34 @@ client.connect()
 
 const addToDatabase = (task) => {
   client.query('INSERT INTO todolist (task) VALUES ($1) RETURNING id', [task])
-    .then(res => {
-      console.log(`Created task ${res.rows[0].id}`)
+    .then(response => {
+      console.log(`Created task ${response.rows[0].id}`)
       client.end()
     })
     .catch(err => console.error(err))
 }
 
-module.exports = { addToDatabase }
+const listAllFromDB = () => {
+  client.query('SELECT * FROM todolist')
+    .then(result => {
+      const widestIDString = Math.max.apply(null, result.rows.map(task => task.id.toString().length))
+
+      if (widestIDString <= 0) {
+        console.log("There are no tasks.")
+        return
+      }
+
+      console.log("ID" + " ".repeat(widestIDString) + "Description")
+      console.log("-".repeat(widestIDString + 1) + " " + "-".repeat(11))
+      result.rows.forEach((task) => {
+        console.log(task.id + " ".repeat(widestIDString + 2 - task.id.toString().length) + task.task)
+      })
+
+      client.end()
+    })
+}
+
+module.exports = {
+  addToDatabase,
+  listAllFromDB
+ }
