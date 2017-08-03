0cd6137543d5ea5eae8d786ed5870ed49964997f
Breyana Scales <breyanaj@gmail.com>
2017-08-03 15:32:30 -0700

    Remove commented out code

--- commands/list.js
+++ commands/list.js
@@ -3,20 +3,6 @@ const { listAllFromDB } = require('../db')
 
 const list = () => {
   listAllFromDB()
-
-    //
-    // const widestIDString = Math.max.apply(null, tasks.map(task => task.id.toString().length))
-    //
-    // if (widestIDString <= 0) {
-    //   console.log("There are no tasks.")
-    //   return
-    // }
-    //
-    // console.log("ID" + " ".repeat(widestIDString) + "Description")
-    // console.log("-".repeat(widestIDString + 1) + " " + "-".repeat(11))
-    // tasks.forEach((task) => {
-    //   console.log(task.id + " ".repeat(widestIDString + 2 - task.id.toString().length) + task.task)
-    // })
 }
 
 module.exports = list
